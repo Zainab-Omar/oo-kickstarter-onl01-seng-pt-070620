9cca666548e74304b984be6d909b6fda09bbe603
Zainab Omar <zainab.alshaikhli01@gmail.com>
2020-08-04 01:09:44 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -8,6 +8,8 @@ class Backer
   end
   def back_project (project)
     @backed_projects << project
-   # binding.pry
+    @backers << self
+    #expect(hoverboard.backers).to include(logan)
+   binding.pry
   end
 end
